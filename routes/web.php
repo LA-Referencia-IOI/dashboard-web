6d3f43c56b897810988b52dd01e73783de9f99e3
EdilsonFilho <edilsonfilhosh@gmail.com>
2024-06-17 16:12:06 +0200

    feat: added frontEnd project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\SiteController;
 use App\Http\Controllers\Dashboard\HomeController;
 use App\Http\Controllers\Dashboard\UserController;
 use App\Http\Controllers\Dashboard\InstitutionController;
@@ -20,12 +20,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => ['https.protocol']], function () {
-    Route::get('/', function () {
-        return redirect('login');
-    });
+    // Route::get('/', function () {
+    //     // return redirect('login');
+    // });
+    Route::get('/', [SiteController::class, 'index'])->name('site.index');
 
     Route::group(['middleware' => ['auth'], 'namespace' => 'Dashboard', 'prefix' => 'dashboard'], function () {
-        Route::get('/', [HomeController::class, 'index'])->name('home.index');
+        Route::get('/login', [HomeController::class, 'index'])->name('home.index');
 
         Route::get('users', [UserController::class, 'index'])->name('users.index');
         Route::get('user/create', [UserController::class, 'create'])->name('users.create');
